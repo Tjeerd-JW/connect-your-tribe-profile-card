cbb37c2af6ac17bbcf540df38a497918d0297245
Tjeerd <tj.wokke@gmail.com>
2026-02-03 13:37:39 +0100

    layout changes #4

--- public/index.js
+++ public/index.js
@@ -4,6 +4,7 @@ async function getPerson() {
     personResponse = await fetch('https://fdnd.directus.app/items/person/256')
     person = await personResponse.json()
     let customData = JSON.parse(person.data.custom)
+    console.log(person)
     console.log(customData)
 
 }
